d4b074d4ccf0c275c848f38114ad0b92c018c168
andylevalley <andylevalley@gmail.com>
2018-11-01 12:17:42 -0400

    Add files via upload

---
+++ Problem.m
@@ -0,0 +1,40 @@
+classdef Problem < handle
+    
+    properties
+        ManeuverList
+        TotalTime
+        Algorithm
+        OptimizationStructure
+        VirtualChiefECI
+    end
+    
+    methods
+        function obj = Problem(ManeuverList,TotalTime,VirtualChief,Algorithm,OptimizationStructure)
+            obj.ManeuverList = ManeuverList;
+            obj.TotalTime = TotalTime;
+            obj.VirtualChiefECI = VirtualChief;
+            obj.Algorithm = Algorithm;
+            obj.OptimizationStructure = OptimizationStructure;
+            
+            NumberManeuvers = length(obj.ManeuverList);
+            NumberVariables = NumberManeuvers*4+2;
+            
+            ub = zeros(1,NumberManeuvers+1);
+            lb = zeros(1,NumberManeuvers+1);
+            ub(1) = obj.TotalTime;
+            lb(1) = 1;
+            for i = 1:NumberManeuvers
+                ub(i+1) = obj.ManeuverList(i).TimeBounds(2);
+                lb(i+1) = obj.ManeuverList(i).TimeBounds(1);
+            end
+
+            obj.OptimizationStructure.NumberVariables = NumberVariables;
+            obj.OptimizationStructure.UpperBound = ub;
+            obj.OptimizationStructure.LowerBound = lb;
+        end
+    end
+    
+    
+end
+    
+        
---
+++ Satellite.m
@@ -0,0 +1,59 @@
+classdef Satellite < handle
+    
+    properties (Constant)
+        mu = 398600.5;     % standard gravitational parameter (km^3/s^2)  
+    end
+    
+    properties (Access = public)
+        Name = 'sat1'               % Satellite type
+        CurrentStateECI             % current satellite state (ECI)
+        CurrentTimeUTCO             % current time (UTCO)
+        InitialStateECI             % initial state (ECI)
+        InitialTimeUTCO             % initial time (UTCO)
+        MeanMotion                  % meanmotion
+    end 
+    
+    methods % initialize class from user
+        function obj = Satellite(Name,InitStateECI,InitTimeUTCO)
+            obj.Name = Name;
+            obj.CurrentStateECI = InitStateECI;
+            obj.CurrentTimeUTCO = InitTimeUTCO;
+            obj.InitialStateECI = InitStateECI;
+            obj.InitialTimeUTCO = InitTimeUTCO;
+            obj.MeanMotion = sqrt(obj.mu/(6378.137 + 35786)^3);
+        end
+    end
+        
+    methods % get current state in Hill frame relative to Virtual Chief
+        function [rHill,vHill] = CurrentStateHill(obj,VirtualChiefECI)
+             [rHill,vHill] = eci2hill(VirtualChiefECI(1:3)',...
+                             VirtualChiefECI(4:6)',...
+                             obj.CurrentStateECI(1:3)',...
+                             obj.CurrentStateECI(4:6)');
+             rHill = rHill';
+             vHill = vHill';
+        end
+    end
+        
+    methods % propagate orbit, update state and current time
+        function obj = Propagate(obj,Time)
+          [rECI,vECI] = TwoBodyPropagation(obj.CurrentStateECI(1:3)',...
+                          obj.CurrentStateECI(4:6)',Time);       
+            obj.CurrentStateECI = [rECI(1:3,end)',vECI(1:3,end)'];
+            obj.CurrentTimeUTCO(6) = obj.CurrentTimeUTCO(6) + Time(end);
+            CurrentTimeJD = utco2jd(obj.CurrentTimeUTCO);
+            obj.CurrentTimeUTCO = jd2utco(CurrentTimeJD);
+        end
+    end
+    
+    methods % get vector from sun to satellite for current satellite state
+        function val = Sun2Rso(obj)
+            sun2RSO_vector = sun2rso(obj.CurrentTimeUTCO,...
+                             obj.CurrentStateECI(1:3)',...
+                             obj.CurrentStateECI(4:6)');
+            val = sun2RSO_vector';
+        end
+    end
+            
+   
+end
