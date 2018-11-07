d093b513188b6c7b1533b0ebcbca1f97c545ebc6
andylevalley <andylevalley@gmail.com>
2018-11-07 07:22:11 -0500

    Add files via upload

--- test_rotations.m
+++ test_rotations.m
@@ -8,36 +8,25 @@ hr0 = 18;
 min0 = 7;
 sec0 = 57.84;
 
-pos = [6378.137 + 35786,0,0];
-vel = [0,7.81599286557539,0];
-sat1 = Satellite('rso1',[pos vel],[yr0,mo0,day0,hr0,min0,sec0]);
+mu = 398600.5;
+sma = 6378.137 + 35786; % semi-major axis of *circular* RSO or virtual RSO (km)
+nu0 = deg2rad(0); % initial true anomaly of RSO or virtual RSO (deg)
+ecc = 0; % eccentricity - this should be or stay very close to zero - we are using the HCW equations!
+incl = deg2rad(0); % inclination (deg) - this combined with RAAN should be appropriate for use (see dissertation)
+RAAN = deg2rad(360); % right ascension of the ascending node (deg) - see note above
+argp = deg2rad(0); % argument of perigee (deg)
+arglat = deg2rad(0); % for ci orbit
+truelon = deg2rad(0); % for ce orbit
+lonper = deg2rad(0); % for ee orbit
+omega = 0;
+w = sqrt(mu/sma)^3;
+p = sma*(1-ecc^2);
 
-% [pos2,vel2] = hill2eci(pos',vel',[0 0 0]',[0 0 0]');
-% sat2 = Satellite('rso1',[pos2' vel2'],[yr0,mo0,day0,hr0,min0,sec0]);
+[rInit,vInit] = coe2rvh(p,ecc,incl,omega,argp,nu0,arglat,truelon,lonper,mu);
 
+sat1 = Satellite('rso1',[rInit' vInit'],[yr0,mo0,day0,hr0,min0,sec0]);
 
-ManeuverInfo = [5, 0, 0, 0, 0, 0];
-maneuver_sat1 = Maneuver('NMC',ManeuverInfo,sat1);
-
-
-[state] = maneuver_sat1.ManeuverStateECI;
-
-[rHillinit(1:3),vHillinit(1:3)] = eci2hill(pos',vel',state(1,1:3)',state(1,4:6)');
-
-% sat2 = Satellite('rso2',state,[yr0,mo0,day0,hr0,min0,sec0]);
-
-sat1.Propagate(60*60);
-[state] = maneuver_sat1.ManeuverStateECI;
-[rHillinit(1:3),vHillinit(1:3)] = eci2hill(sat1.CurrentStateECI(1:3)',sat1.CurrentStateECI(4:6)',state(1,1:3)',state(1,4:6)');
-
-% for i = 1:12
-%     [rHill(1:3,i),vHill(1:3,i)] = eci2hill(pos',vel',stateNMC(i,1:3)',stateNMC(i,4:6)');
-%     
-% end
-
-[rHill,vHill] = CWHPropagator(rHillinit',vHillinit',sat1.MeanMotion,0:60:24*60*60);
-
-scatter(rHill(2,:),rHill(1,:))
+sat1.Propagate(23*60*60 + 53*60 + 4)
     
     
     
